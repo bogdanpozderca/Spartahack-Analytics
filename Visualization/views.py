8e38605411718d254f5dea5ab1a22ff03d1dafd7
raddingy@msu.edu <ht7854@ht7854-Precision-T1500.(none)>
2015-02-23 14:10:57 -0500

    fixed api things

--- Visualization/views.py
+++ Visualization/views.py
@@ -9,7 +9,7 @@ def index(request):
 	typeFormURL = "https://api.typeform.com/v0/form/Zh7TEH?key=1b508a88a1331cf2c182cb136f6da1ebd880a299&completed=true"
 	jsonResults = ""
 	for i in urlopen(typeFormURL):
-		jsonResults + str(i)
+		jsonResults += str(i)
 	jsonResults = json.loads(jsonResults)
 	context = {}
 	return render(request, 'Visualization/index.html', context)
