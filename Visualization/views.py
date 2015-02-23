5f11680cf33e847bfc8e89c365d68f05e360ed42
raddingy@msu.edu <ht7854@ht7854-Precision-T1500.(none)>
2015-02-23 14:03:52 -0500

    added api things

--- Visualization/views.py
+++ Visualization/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse, Http404, JsonResponse
+from urllib import urlopen
+import json
 
 # Create your views here.
 
 def index(request):
+	typeFormURL = "https://api.typeform.com/v0/form/Zh7TEH?key=1b508a88a1331cf2c182cb136f6da1ebd880a299&completed=true"
+	jsonResults = ""
+	for i in urlopen(typeFormURL):
+		jsonResults + str(i)
+	jsonResults = json.loads(jsonResults)
 	context = {}
 	return render(request, 'Visualization/index.html', context)
